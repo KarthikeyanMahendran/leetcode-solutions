cb2578530822d3d982dec8e8b314f4b335aa9abf
KarthikeyanMahendran <karthi13122005@gmail.com>
2026-07-13 23:32:38 +0530

    solve: #242 Valid Anagram [Easy]

---
+++ LeetCode/valid_anagram.java
@@ -0,0 +1,27 @@
+// Valid Anagram [Easy]
+// https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+
+        }
+        int count[] = new int[26];
+        for (int i =0; i<s.length(); i++){
+            count[s.charAt(i)- 'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+
+        for(int c : count){
+            if(c !=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+    
+
+
+            
