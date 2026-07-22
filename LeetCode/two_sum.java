0b9d827365186dada5252dc88ce9b503ceec93fc
KarthikeyanMahendran <karthi13122005@gmail.com>
2026-07-23 01:45:24 +0530

    solve: #1 Two Sum [Easy]

---
+++ LeetCode/two_sum.java
@@ -0,0 +1,18 @@
+// Two Sum [Easy]
+// https://leetcode.com/problems/two-sum/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int i =0; i< nums.length; i++){
+            int c = target - nums[i];
+            if(mp.containsKey(c)){
+                return new int[] {mp.get(c),i};
+            }
+            mp.put(nums[i],i);
+            
+        }
+        return new int[]{};
+        
+    }
+}
