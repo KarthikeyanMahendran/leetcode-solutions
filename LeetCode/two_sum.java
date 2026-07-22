a407fb6e92c4b2cb5dc74b45b90a2d8693e9962f
KarthikeyanMahendran <karthi13122005@gmail.com>
2026-07-23 01:56:52 +0530

    solve: #1 Two Sum [Easy]

--- LeetCode/two_sum.java
+++ LeetCode/two_sum.java
@@ -3,16 +3,17 @@
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        for (int i =0; i< nums.length; i++){
-            int c = target - nums[i];
-            if(mp.containsKey(c)){
-                return new int[] {mp.get(c),i};
+        for (int i = 0;i< nums.length - 1; i++){   
+            for (int j = i+1; j< nums.length; j++){
+                int sum = nums[i]+ nums[j];
+                if( sum == target){
+                    return new int[]{i,j};
+                }
+             
             }
-            mp.put(nums[i],i);
-            
-        }
-        return new int[]{};
         
+
+
     }
-}
+    return new int[]{};
+}}
