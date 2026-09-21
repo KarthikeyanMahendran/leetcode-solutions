edd75d437b575a1479e14c98c34ece0f48a6fc93
KarthikeyanMahendran <karthi13122005@gmail.com>
2026-09-21 12:56:09 +0530

    solve: #268 Missing Number [Easy]

--- LeetCode/missing_number.java
+++ LeetCode/missing_number.java
@@ -3,15 +3,18 @@
 
 class Solution {
     public int missingNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums){
-            set.add(num);
+        int ans;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i< nums.length; i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        for (int i =0; i<= nums.length; i++){
-            if(!set.contains(i)){
-                return i;
-            }
+       for (int i =0; i<= nums.length ; i++){
+        if(!map.containsKey(i)){
+            return i ;
         }
-        return -1;
+       }
+
+
+       return -1;
     }
 }
