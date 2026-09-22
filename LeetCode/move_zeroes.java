229b1fadc6e2fd2505710b63a25e5a01b2eae0aa
KarthikeyanMahendran <karthi13122005@gmail.com>
2026-09-22 08:47:33 +0530

    solve: #283 Move Zeroes [Easy]

--- LeetCode/move_zeroes.java
+++ LeetCode/move_zeroes.java
@@ -3,17 +3,21 @@
 
 class Solution {
     public void moveZeroes(int[] nums) {
-        int k = 0 ;   
-        for ( int i = 0 ; i < nums.length ; i++ ) {
-            if ( nums[i] != 0 ) {
-                nums[k] = nums[i] ;   
-                k ++ ;   
+        int j =0;
+        for (int i =0; i< nums.length; i++){
+            if(nums[i]!=0){
+                nums[j]= nums[i];
+                j++;
+                
+
             }
+            
         }
-        while ( k < nums.length ) {
-            nums[k] = 0 ;   
-            k ++ ;   
+        while(j<nums.length){
+            nums[j] = 0;
+            j++;
         }
+
         
     }
 }
