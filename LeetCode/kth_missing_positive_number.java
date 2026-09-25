e31117c33555a532683d561c7b944c536c894524
KarthikeyanMahendran <karthi13122005@gmail.com>
2026-09-26 01:29:26 +0530

    solve: #1646 Kth Missing Positive Number [Easy]

---
+++ LeetCode/kth_missing_positive_number.java
@@ -0,0 +1,21 @@
+// Kth Missing Positive Number [Easy]
+// https://leetcode.com/problems/kth-missing-positive-number/
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        for(int i : arr){
+            if(i<=k){
+                k++;
+            }
+            else{
+
+            break;}
+
+
+        }
+        
+        
+    return k;
+    }
+    
+}
