f6cbdc5d3bda9ff87a836b47e9dc3fed08e9c9b8
KarthikeyanMahendran <karthi13122005@gmail.com>
2026-09-26 00:43:31 +0530

    solve: #1908 Recyclable and Low Fat Products [Easy]

---
+++ LeetCode/recyclable_and_low_fat_products.sql
@@ -0,0 +1,4 @@
+// Recyclable and Low Fat Products [Easy]
+// https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+Select product_id from Products where low_fats = 'Y' and recyclable = 'Y';
