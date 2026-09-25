6a882d7f24a226f5d0075be3b1842cddc2997389
KarthikeyanMahendran <karthi13122005@gmail.com>
2026-09-26 01:31:15 +0530

    solve: #584 Find Customer Referee [Easy]

---
+++ LeetCode/find_customer_referee.sql
@@ -0,0 +1,8 @@
+// Find Customer Referee [Easy]
+// https://leetcode.com/problems/find-customer-referee/
+
+# Write your MySQL query statement below
+select name
+from Customer
+where referee_id !=2
+or referee_id is null;
